8d665f896f8685d662df66f0f16d4a6a2a606d1f
Denzkristangacusan <gdenzkristan@gmail.com>
2024-11-04 17:58:08 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,8 @@
+
+let subjectTitle = "English 101"; 
+let classSchedule = "8:00 AM - 9:30 AM, Mondays and Wednesdays"; 
+let classroom = "COM LAB A"; 
+let classInstructor = "MRS.GACUSAN"; 
+let studentName = "DENZ KRISTAN"; 
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
